9c40bf6bc883824147fe58b3a53a8596ff166af0
Samuel <54447830+Ausaci@users.noreply.github.com>
2020-10-02 14:55:00 +0800

    Update diy-part1.sh
     Add jd-dailybonus

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean
