5ae716fd02427e9ee61cdaa01f12c7ba635ff983
Samuel <54447830+Ausaci@users.noreply.github.com>
2022-12-14 22:10:49 +0800

    Add luci-app-adguardhome

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,33 +16,36 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+# Add luci-app-adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+
+# Add Netmap
+git clone https://github.com/Ausaci/luci-app-netmap package/netmap
+
 # Add Nat6-helper
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git package/luci-app-nat6-helper
+
+# Add ServerChan
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 
 # Add helloworld fw876
 git clone https://github.com/fw876/helloworld package/helloworld
 
 # Add helloworld_Copy (V2ray-core version 180-10 at Nov 28, 2020)
-#git clone https://github.com/Ausaci/helloworld_Copy.git package/helloworld-copy
+# git clone https://github.com/Ausaci/helloworld_Copy.git package/helloworld-copy
 
 # Add luci-app-vssr
-#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+# git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 
 # Add luci-app-shadowsocks
-git clone https://github.com/shadowsocks/luci-app-shadowsocks.git package/luci-app-shadowsocks
+# git clone https://github.com/shadowsocks/luci-app-shadowsocks.git package/luci-app-shadowsocks
 
-# Add luci-app-socat
-#svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-socat package/luci-app-socat
-
-# Add ServerChan
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-
-# Add Netmap
-git clone https://github.com/Ausaci/luci-app-netmap package/netmap
+# Add luci-app-socat (Added in lean/luci)
+# svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-socat package/luci-app-socat
 
 # Add Aliddns
-git clone https://github.com/Ausaci/luci-app-aliddns package/aliddns
+# git clone https://github.com/Ausaci/luci-app-aliddns package/aliddns
 
 # Modify Makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/luci\.mk/include \$(TOPDIR)\/feeds\/luci\/luci\.mk/g' {}
